d9a4c6c02c17e963efba58a944ebc0ab64791fd1
Fernando Spelling <fernando.spelling@linx.com.br>
2019-07-15 19:53:34 -0300

    .

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -1,17 +1,61 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
+const htmlPlugin = require('html-webpack-plugin');
 
-let plugins = [];
+let plugins = [
+    new htmlPlugin({
+        hash: true,
+        minify: {
+            html5: true,
+            collapseWhitespace: true,
+            removeComments: true
+        },
+        filename: 'index.html',
+        template: __dirname + '/main.html'
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new extractTextPlugin('styles.css'),
+    new webpack.ProvidePlugin({
+        $: 'jquery/dist/jquery.js',
+        jQuery: 'jquery/dist/jquery.js'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.bundle.js'
+    })
+];
 
-if(process.env.NODE_ENV === 'production')
+let SERVICE_URL = JSON.stringify('http//localhost:3000');
+
+if (process.env.NODE_ENV === 'production') {
+    SERVICE_URL = JSON.stringify('http//api.prod');
+
     plugins.push(new babiliPlugin());
 
+    plugins.push(new optimizeCssAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        canPrint: true,
+        cssProcessorOptions: {
+            discardComments: {
+                removeAll: true
+            }
+        }
+    }));
+}
+
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }));
+
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
     },
     module: {
         rules: [
@@ -21,6 +65,29 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: extractTextPlugin.extract({
+                    use: 'css-loader',
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
             }
         ]
     },
